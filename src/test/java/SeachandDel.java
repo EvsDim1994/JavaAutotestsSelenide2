c8fd604ff67533d321378253d38b364835e4a6d8
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-07-14 17:52:49 +0300

    Коммит по удалению пользователя

---
+++ src/test/java/SeachandDel.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import io.restassured.internal.common.assertion.Assertion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SeachandDel extends Hooks {
+    @Test
+    public void test(){
+        $x("//button[contains(text(),'Bank Manager Login')]").shouldBe(Condition.visible).click();
+        $x("//button[contains(text(),'Customers')]").shouldBe(Condition.visible).click();
+        String name = "Hermoine";
+        $x("//input[@placeholder='Search Customer']").sendKeys(name);
+        String postcode = $x("//tbody/tr/td[3]").getText();
+        String postcode1 = "E859AB";
+        Assertions.assertEquals(postcode, postcode1);
+
+        try {
+            Thread.sleep(4000);
+            $x("//button[contains(text(),'Delete')]").shouldBe(Condition.visible).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
