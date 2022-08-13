10943e96d8db04240db9128c79882dd268b78a36
EvsDim1994 <evsyuckov.dmitry@yandex.ru>
2022-08-13 16:53:36 +0300

    Новый коммит от 13.08

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,10 +1,8 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class Hooks {
@@ -13,14 +11,12 @@ public class Hooks {
     public void tearDown() {
         Configuration.reportsFolder = "ScreenShots";
         Configuration.timeout = 20000;
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        WebDriverRunner.setWebDriver(webDriver);
         Selenide.open("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+        Selenide.webdriver().driver().getWebDriver().manage().window().maximize();
     }
 
     @AfterEach
     public void doAfter() {
-        WebDriverRunner.getWebDriver().quit();
+        Selenide.closeWebDriver();
     }
 }
